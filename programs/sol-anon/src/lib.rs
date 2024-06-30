aaaa5855fe5175e21b84f141429ed59435874715
idatsy <stefan@ranvier.co.uk>
2024-06-30 17:52:44 +0100

    refactor: first pass cleaning up program code

--- programs/sol-anon/src/lib.rs
+++ programs/sol-anon/src/lib.rs
@@ -2,6 +2,12 @@ use anchor_lang::prelude::*;
 
 declare_id!("73o3wfunMxS44tEuia2D4WJKmJtqRewX3F4tG2fmkSEV");
 
+// Constants for seeds and space calculations
+const INBOX_SEED: &[u8] = b"inbox";
+const INBOX_SPACE: usize = 8 + 32 + 8 + 8; // discriminator + pubkey + 2 * u64
+const WHITELIST_SPACE: usize = 8; // discriminator
+const SLOT_BASE_SPACE: usize = 8 + 32 + 4; // discriminator + pubkey + String prefix
+
 #[program]
 pub mod sol_anon {
     use super::*;
@@ -20,7 +26,7 @@ pub mod sol_anon {
 
     pub fn change_admin(ctx: Context<ChangeAdmin>, new_admin: Pubkey) -> Result<()> {
         let inbox = &mut ctx.accounts.inbox;
-        inbox.admin = new_admin.key().clone();
+        inbox.admin = new_admin;
 
         msg!("Inbox admin changed to: {:?}", inbox.admin);
 
@@ -52,49 +58,14 @@ pub mod sol_anon {
     pub fn send_whitelisted_message(ctx: Context<SendWhitelistedMessage>, message: String, to: Pubkey) -> Result<()> {
         let inbox = &mut ctx.accounts.inbox;
 
-        let slot = &mut ctx.accounts.slot.to_account_info();
-        let current_space = slot.to_account_info().data_len();
-        let required_space = 8 + 32 + 4 + message.len();
-
-        slot.realloc(required_space, false)?;
-
-        let rent = Rent::get()?;
-        let new_rent = rent.minimum_balance(required_space);
-        let old_rent = rent.minimum_balance(current_space);
-
-        if new_rent > old_rent {
-            // transfer from the signer to the slot
-            let diff = new_rent.saturating_sub(old_rent);
-            msg!("Transferring {:?} lamports to slot", diff);
-            // try to take this from the inbox buffer first
-            let inbox_rent = rent.minimum_balance(inbox.to_account_info().data_len());
-            let inbox_surplus = inbox.to_account_info().lamports().saturating_sub(inbox_rent);
-
-            let remaining_diff = diff.saturating_sub(inbox_surplus);
-            msg!("Remaining diff: {:?}", remaining_diff);
-            if remaining_diff > 0 {
-                let transfer_instruction = solana_program::system_instruction::transfer(&ctx.accounts.sender.key(), &slot.key(), remaining_diff);
-                msg!("Transferring {:?} lamports to slot", remaining_diff);
-                solana_program::program::invoke_signed(
-                    &transfer_instruction,
-                    &[
-                        ctx.accounts.sender.to_account_info(),
-                        slot.to_account_info(),
-                        ctx.accounts.system_program.to_account_info(),
-                    ],
-                    &[],
-                )?;
-            }
-            // DEV: We have to put PDA transfers after native transfers for *reasons*. See: https://solana.stackexchange.com/questions/4519/anchor-error-error-processing-instruction-0-sum-of-account-balances-before-and
-            if inbox_surplus > 0 {
-                **inbox.to_account_info().try_borrow_mut_lamports()? -= inbox_surplus;
-                **slot.try_borrow_mut_lamports()? += inbox_surplus;
-            }
-        } else if new_rent < old_rent {
-            let diff = old_rent - new_rent;
-            **slot.try_borrow_mut_lamports()? -= diff;
-            **inbox.to_account_info().try_borrow_mut_lamports()? += diff;
-        }
+        // Reallocate the slot if necessary
+        realloc_slot(
+            &ctx.accounts.slot.to_account_info(),
+            &message,
+            inbox,
+            &ctx.accounts.sender.to_account_info(),
+            &ctx.accounts.system_program.to_account_info(),
+        )?;
 
         let slot = &mut ctx.accounts.slot;
         slot.to = to;
@@ -112,19 +83,70 @@ pub mod sol_anon {
     }
 
     pub fn withdraw_surplus_inbox_balance(ctx: Context<WithdrawSurplusInboxBalance>) -> Result<()> {
-        let inbox = &mut ctx.accounts.inbox;
+        let inbox = &ctx.accounts.inbox;
+        let admin = &ctx.accounts.admin;
 
         let rent = Rent::get()?;
-        let rent = rent.minimum_balance(inbox.to_account_info().data_len());
-        let surplus = inbox.to_account_info().lamports() - rent;
+        let minimum_balance = rent.minimum_balance(inbox.to_account_info().data_len());
+        let surplus = inbox.to_account_info().lamports().saturating_sub(minimum_balance);
 
         **inbox.to_account_info().try_borrow_mut_lamports()? -= surplus;
-        **ctx.accounts.admin.to_account_info().try_borrow_mut_lamports()? += surplus;
+        **admin.to_account_info().try_borrow_mut_lamports()? += surplus;
 
         msg!("Surplus of {:?} lamports withdrawn from inbox", surplus);
         Ok(())
     }
 }
+
+/// Reallocates the slot account if necessary and handles the transfer of lamports
+fn realloc_slot<'a>(
+    slot: &AccountInfo<'a>,
+    message: &str,
+    inbox: &mut Account<'a, Inbox>,
+    sender: &AccountInfo<'a>,
+    system_program: &AccountInfo<'a>,
+) -> Result<()> {
+    let current_space = slot.data_len();
+    let required_space = SLOT_BASE_SPACE + message.len();
+
+    slot.realloc(required_space, false)?;
+
+    let rent = Rent::get()?;
+    let new_rent = rent.minimum_balance(required_space);
+    let old_rent = rent.minimum_balance(current_space);
+
+    if new_rent > old_rent {
+        let diff = new_rent.saturating_sub(old_rent);
+        let inbox_rent = rent.minimum_balance(inbox.to_account_info().data_len());
+        let inbox_surplus = inbox.to_account_info().lamports().saturating_sub(inbox_rent);
+
+        let remaining_diff = diff.saturating_sub(inbox_surplus);
+        if remaining_diff > 0 {
+            let transfer_instruction = solana_program::system_instruction::transfer(
+                sender.key,
+                slot.key,
+                remaining_diff
+            );
+            solana_program::program::invoke(
+                &transfer_instruction,
+                &[sender.clone(), slot.clone(), system_program.clone()],
+            )?;
+        }
+
+        // DEV: We have to put PDA transfers after native transfers for *reasons*. See: https://solana.stackexchange.com/questions/4519/anchor-error-error-processing-instruction-0-sum-of-account-balances-before-and
+        if inbox_surplus > 0 {
+            **inbox.to_account_info().try_borrow_mut_lamports()? -= inbox_surplus;
+            **slot.try_borrow_mut_lamports()? += inbox_surplus;
+        }
+    } else if new_rent < old_rent {
+        let diff = old_rent - new_rent;
+        **slot.try_borrow_mut_lamports()? -= diff;
+        **inbox.to_account_info().try_borrow_mut_lamports()? += diff;
+    }
+
+    Ok(())
+}
+
 
 #[account]
 pub struct Slot {
@@ -137,7 +159,13 @@ pub struct Slot {
 pub struct SendRegularMessage<'info> {
     #[account(mut)]
     pub inbox: Account<'info, Inbox>,
-    #[account(init, seeds=[&inbox.latest_free_slot.to_le_bytes()], bump, payer = sender, space = 8 + 32 + 4 + message.len())]
+    #[account(
+        init,
+        seeds=[&inbox.latest_free_slot.to_le_bytes()],
+        bump,
+        payer = sender,
+        space = SLOT_BASE_SPACE + message.len()
+    )]
     pub slot: Account<'info, Slot>,
     #[account(mut)]
     pub sender: Signer<'info>,
@@ -198,17 +226,22 @@ pub struct Inbox {
 
 #[derive(Accounts)]
 pub struct InitializeInbox<'info> {
-    #[account(init, seeds=[b"inbox"], bump, payer = admin, space = 8 + 32 + 8 + 8)]
+    #[account(
+        init,
+        seeds=[INBOX_SEED],
+        bump,
+        payer = admin,
+        space = INBOX_SPACE
+    )]
     pub inbox: Account<'info, Inbox>,
     #[account(mut)]
     pub admin: Signer<'info>,
     pub system_program: Program<'info, System>,
 }
 
-
 #[derive(Accounts)]
 pub struct ChangeAdmin<'info> {
-    #[account(mut, seeds=[b"inbox"], bump, has_one = admin)]
+    #[account(mut, seeds=[INBOX_SEED], bump, has_one = admin)]
     pub inbox: Account<'info, Inbox>,
     pub admin: Signer<'info>,
 }
@@ -217,13 +250,19 @@ pub struct ChangeAdmin<'info> {
 pub struct Whitelist {}
 
 #[derive(Accounts)]
-#[instruction(address_to_whitelist: Pubkey)] // We could make this an account but showcasing how to do this for reference ^.^
+#[instruction(address_to_whitelist: Pubkey)]
 pub struct AddToWhitelist<'info> {
     // TODO: If time allows, don't use anchor so we can skip the 8 byte discriminant and save another 7 bytes of space!
     // Also would be nice to have a better understanding of PDA initialization, for example seeding a PDA with rent excemption alone is not secure
     // as there *could* be a way to to send money to a non-existing PDA even though it hasn't been created by this admin.
     // Either way due to time contsraints this probably won't be possible to do with Anchor but interesting to explore using a native implementation
-    #[account(init, seeds=[address_to_whitelist.key().as_ref()], bump, payer = admin, space = 8)]
+    #[account(
+        init,
+        seeds=[address_to_whitelist.key().as_ref()],
+        bump,
+        payer = admin,
+        space = WHITELIST_SPACE
+    )]
     pub whitelist_pda: Account<'info, Whitelist>,
     #[account(mut)]
     pub admin: Signer<'info>,
