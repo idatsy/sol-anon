c6e6f2b401451e1d7d502be3128021eb7b898ec8
idatsy <stefan@ranvier.co.uk>
2024-06-30 17:48:34 +0100

    refactor: clean-up tests to reduce duplication and improve readability

--- tests/sol-anon.ts
+++ tests/sol-anon.ts
@@ -3,151 +3,140 @@ import { Program } from "@coral-xyz/anchor";
 import { SolAnon } from "../target/types/sol_anon";
 import { expect } from "chai";
 
+
 describe("sol-anon", () => {
+  // Setup
   const provider = anchor.AnchorProvider.env();
   anchor.setProvider(provider);
-
   const program = anchor.workspace.SolAnon as Program<SolAnon>;
 
-  // inital owner
+  // Test accounts
   const owner = anchor.web3.Keypair.generate();
-  // new owner after testing ownership change
   const newOwner = anchor.web3.Keypair.generate();
-  // whitelisted user
   const whitelistedSender = anchor.web3.Keypair.generate();
-  // regular user
   const nonWhitelistedSender = anchor.web3.Keypair.generate();
 
   const [inbox] = anchor.web3.PublicKey.findProgramAddressSync(
-      [Buffer.from("inbox")], program.programId
+    [Buffer.from("inbox")],
+    program.programId
   );
 
+  // Helper functions
+  async function airdropSol(to: anchor.web3.PublicKey, amount: number) {
+    const airdrop = await provider.connection.requestAirdrop(to, amount);
+    await provider.connection.confirmTransaction(airdrop);
+  }
+
+  async function getInboxAccount() {
+    return await program.account.inbox.fetch(inbox);
+  }
+
+  async function getSlotAccount(slot: number) {
+    const slotBuffer = Buffer.alloc(8);
+    slotBuffer.writeBigUInt64LE(BigInt(slot));
+    const [slotPda] = anchor.web3.PublicKey.findProgramAddressSync(
+      [slotBuffer],
+      program.programId
+    );
+    return await program.account.slot.fetch(slotPda);
+  }
+
+  // Tests
   it("Initializes an inbox", async () => {
-      let token_airdrop = await provider.connection.requestAirdrop(owner.publicKey, 1000000000);
-      await provider.connection.confirmTransaction(token_airdrop);
+    await airdropSol(owner.publicKey, 1000000000);
 
-      await program
-          .methods
-          .initialize()
-          .accountsPartial({admin: owner.publicKey})
-          .signers([owner])
-          .rpc();
+    await program.methods
+      .initialize()
+      .accountsPartial({ admin: owner.publicKey })
+      .signers([owner])
+      .rpc();
 
-        let inboxAccount = await program.account.inbox.fetch(inbox);
-        expect(inboxAccount.admin.toString() === owner.publicKey.toString());
+    const inboxAccount = await getInboxAccount();
+    expect(inboxAccount.admin.toString()).to.equal(owner.publicKey.toString());
   });
 
   it("Changes owner", async () => {
-    await program
-        .methods
-        .changeAdmin(newOwner.publicKey)
-        .accountsPartial({admin: owner.publicKey})
-        .signers([owner])
-        .rpc();
+    await program.methods
+      .changeAdmin(newOwner.publicKey)
+      .accountsPartial({ admin: owner.publicKey })
+      .signers([owner])
+      .rpc();
 
-    let inboxAccount = await program.account.inbox.fetch(inbox);
+    const inboxAccount = await getInboxAccount();
     expect(inboxAccount.admin.toString()).to.equal(newOwner.publicKey.toString());
   });
 
   it("Adds a user to whitelist", async () => {
-    let token_airdrop = await provider.connection.requestAirdrop(newOwner.publicKey, 1000000000);
-    await provider.connection.confirmTransaction(token_airdrop);
+    await airdropSol(newOwner.publicKey, 1000000000);
 
-    let sig = await program
-        .methods
-        .addToWhitelist(whitelistedSender.publicKey)
-        .accountsPartial({admin: newOwner.publicKey})
-        .signers([newOwner])
-        .rpc();
+    await program.methods
+      .addToWhitelist(whitelistedSender.publicKey)
+      .accountsPartial({ admin: newOwner.publicKey })
+      .signers([newOwner])
+      .rpc();
 
-    const [excpected_pda] = anchor.web3.PublicKey.findProgramAddressSync([whitelistedSender.publicKey.toBuffer()], program.programId);
-    const account_info = await provider.connection.getAccountInfo(excpected_pda);
-    expect(account_info).to.not.be.null;
+    const [expectedPda] = anchor.web3.PublicKey.findProgramAddressSync(
+      [whitelistedSender.publicKey.toBuffer()],
+      program.programId
+    );
+    const accountInfo = await provider.connection.getAccountInfo(expectedPda);
+    expect(accountInfo).to.not.be.null;
   });
 
   it("Non-whitelisted user can't send a message as whitelisted", async () => {
-    const nonWhitelistedSender = anchor.web3.Keypair.generate();
-    const [whitelisted_pda] = anchor.web3.PublicKey.findProgramAddressSync([whitelistedSender.publicKey.toBuffer()], program.programId);
-
+    const [whitelistedPda] = anchor.web3.PublicKey.findProgramAddressSync(
+      [whitelistedSender.publicKey.toBuffer()],
+      program.programId
+    );
     try {
-      await program.methods
-        .sendWhitelistedMessage("Hello World!", owner.publicKey)
-        .accountsPartial({ whitelist: whitelisted_pda, sender: nonWhitelistedSender.publicKey })
-        .signers([nonWhitelistedSender])
-        .rpc();
-
-      expect.fail("Expected transaction to fail, but it succeeded");
-    } catch (error) {
-      expect(error, "Transaction should have failed");
+      await program
+          .methods
+          .sendWhitelistedMessage("Hello World!", owner.publicKey)
+          .accountsPartial({ whitelist: whitelistedPda, sender: nonWhitelistedSender.publicKey })
+          .signers([nonWhitelistedSender])
+          .rpc()
+    } catch (err) {
+        expect(err).to.exist;
     }
   });
 
   it("Non whitelisted user can send a regular message", async () => {
-    let token_airdrop = await provider.connection.requestAirdrop(nonWhitelistedSender.publicKey, 1000000000);
-    await provider.connection.confirmTransaction(token_airdrop);
+    await airdropSol(nonWhitelistedSender.publicKey, 1000000000);
 
-    await program
-        .methods
-        .sendRegularMessage("Hello World!", owner.publicKey)
-        .accountsPartial({inbox: inbox, sender: nonWhitelistedSender.publicKey})
-        .signers([nonWhitelistedSender])
-        .rpc();
+    await program.methods
+      .sendRegularMessage("Hello World!", owner.publicKey)
+      .accountsPartial({ inbox: inbox, sender: nonWhitelistedSender.publicKey })
+      .signers([nonWhitelistedSender])
+      .rpc();
 
-    // check that messages have been incremented
-    let inboxAccount = await program.account.inbox.fetch(inbox);
+    const inboxAccount = await getInboxAccount();
     expect(inboxAccount.latestFreeSlot.toString()).to.equal("1");
 
-    // look up the slot and check the message is correct
-    // create a buffer to derive the PDA same way as the program
-    const latestFreeSlotBuffer = Buffer.alloc(8);
-    latestFreeSlotBuffer.writeBigUInt64LE(BigInt(0));
-    const [slotPda] = anchor.web3.PublicKey.findProgramAddressSync(
-        [latestFreeSlotBuffer],
-        program.programId
-    );
-    // get the slot data
-    const slotAccount = await program.account.slot.fetch(slotPda);
-    // check it matches the expected data
+    const slotAccount = await getSlotAccount(0);
     expect(slotAccount.message).to.equal("Hello World!");
     expect(slotAccount.to.toString()).to.equal(owner.publicKey.toString());
   });
 
   it("Whitelisted user can send a message without paying for the slot and inbox refunded rent difference", async () => {
-    let token_airdrop = await provider.connection.requestAirdrop(whitelistedSender.publicKey, 1000000000);
-    await provider.connection.confirmTransaction(token_airdrop);
+    await airdropSol(whitelistedSender.publicKey, 1000000000);
 
-    // Get the initial balance of the inbox so that we can check if it received a refund
-    const initialInboxBalance = await program.provider.connection.getBalance(inbox);
+    const initialInboxBalance = await provider.connection.getBalance(inbox);
 
-    // now that a slot has been paid for by the non-whitelisted user, the whitelisted user can send a message without paying
-    await program
-        .methods
-        .sendWhitelistedMessage("Hi!", newOwner.publicKey)
-        .accountsPartial({inbox: inbox, sender: whitelistedSender.publicKey})
-        .signers([whitelistedSender])
-        .rpc();
+    await program.methods
+      .sendWhitelistedMessage("Hi!", newOwner.publicKey)
+      .accountsPartial({ inbox: inbox, sender: whitelistedSender.publicKey })
+      .signers([whitelistedSender])
+      .rpc();
 
-    // check that messages have been incremented
-    let inboxAccount = await program.account.inbox.fetch(inbox);
+    const inboxAccount = await getInboxAccount();
     expect(inboxAccount.latestFreeSlot.toString()).to.equal("1");
     expect(inboxAccount.latestWhitelistedSlot.toString()).to.equal("1");
 
-    // look up the slot and check the message is correct
-    // create a buffer to derive the PDA same way as the program
-    const latestFreeSlotBuffer = Buffer.alloc(8);
-    latestFreeSlotBuffer.writeBigUInt64LE(BigInt(0));
-    const [slotPda] = anchor.web3.PublicKey.findProgramAddressSync(
-        [latestFreeSlotBuffer],
-        program.programId
-    );
-    // get the slot data
-    const slotAccount = await program.account.slot.fetch(slotPda);
-    // check it matches the expected data
+    const slotAccount = await getSlotAccount(0);
     expect(slotAccount.message).to.equal("Hi!");
     expect(slotAccount.to.toString()).to.equal(newOwner.publicKey.toString());
 
-    // check that the rent has been refunded to the inbox account
-    const finalInboxBalance = await program.provider.connection.getBalance(inbox);
+    const finalInboxBalance = await provider.connection.getBalance(inbox);
     expect(finalInboxBalance).to.be.greaterThan(initialInboxBalance);
   });
 
@@ -156,88 +145,81 @@ describe("sol-anon", () => {
       await program
           .methods
           .sendWhitelistedMessage("This should fail!", newOwner.publicKey)
-          .accountsPartial({inbox: inbox, sender: whitelistedSender.publicKey})
+          .accountsPartial({ inbox: inbox, sender: whitelistedSender.publicKey })
           .signers([whitelistedSender])
           .rpc();
-
-      expect.fail("Expected transaction to fail, but it succeeded");
-    } catch (error) {
-        expect(error, "Transaction should have failed");
+    } catch (err) {
+      expect(err).to.exist;
     }
   });
 
   it("Whitelisted user can send a message and pay for the slot realloc", async () => {
-    // send a non-whitelisted message to create a slot
-    await program
-        .methods
-        .sendRegularMessage("Short message here", owner.publicKey)
-        .accountsPartial({inbox: inbox, sender: nonWhitelistedSender.publicKey})
-        .signers([nonWhitelistedSender])
-        .rpc();
+    await program.methods
+      .sendRegularMessage("Short message here", owner.publicKey)
+      .accountsPartial({ inbox: inbox, sender: nonWhitelistedSender.publicKey })
+      .signers([nonWhitelistedSender])
+      .rpc();
 
-    // now that a slot has been paid for by the non-whitelisted user, the whitelisted user can send a message paying only for the difference
-    let sig = await program
-        .methods
-        .sendWhitelistedMessage("This is a much much longer message that will require more space", owner.publicKey)
-        .accountsPartial({inbox: inbox, sender: whitelistedSender.publicKey})
-        .signers([whitelistedSender])
-        .rpc();
+    await program.methods
+      .sendWhitelistedMessage("This is a much much longer message that will require more space", owner.publicKey)
+      .accountsPartial({ inbox: inbox, sender: whitelistedSender.publicKey })
+      .signers([whitelistedSender])
+      .rpc();
 
-    // check that messages have been incremented
-    let inboxAccount = await program.account.inbox.fetch(inbox);
+    const inboxAccount = await getInboxAccount();
     expect(inboxAccount.latestWhitelistedSlot.toString()).to.equal("2");
   });
 
   it("Removes a user from the whitelist", async () => {
-    const [excpected_pda] = anchor.web3.PublicKey.findProgramAddressSync([whitelistedSender.publicKey.toBuffer()], program.programId);
-    const account_info = await provider.connection.getAccountInfo(excpected_pda);
-    expect(account_info).to.not.be.null;
+    const [expectedPda] = anchor.web3.PublicKey.findProgramAddressSync(
+      [whitelistedSender.publicKey.toBuffer()],
+      program.programId
+    );
 
-    await program
-        .methods
-        .removeFromWhitelist(whitelistedSender.publicKey)
-        .accountsPartial({admin: newOwner.publicKey})
-        .signers([newOwner])
-        .rpc();
+    const accountInfoBefore = await provider.connection.getAccountInfo(expectedPda);
+    expect(accountInfoBefore).to.not.be.null;
 
-    const account_info_after = await provider.connection.getAccountInfo(excpected_pda);
-    expect(account_info_after).to.be.null;
+    await program.methods
+      .removeFromWhitelist(whitelistedSender.publicKey)
+      .accountsPartial({ admin: newOwner.publicKey })
+      .signers([newOwner])
+      .rpc();
+
+    const accountInfoAfter = await provider.connection.getAccountInfo(expectedPda);
+    expect(accountInfoAfter).to.be.null;
   });
 
   it("Admin can reclaim slots", async () => {
-    const latestFreeSlotBuffer = Buffer.alloc(8);
-    latestFreeSlotBuffer.writeBigUInt64LE(BigInt(0));
     const [slotPda] = anchor.web3.PublicKey.findProgramAddressSync(
-        [latestFreeSlotBuffer],
-        program.programId
+      [Buffer.alloc(8)],
+      program.programId
     );
 
-    await program
-        .methods
-        .reclaimSlot()
-        .accounts({inbox: inbox, slot: slotPda, admin: newOwner.publicKey})
-        .signers([newOwner])
-        .rpc();
+    await program.methods
+      .reclaimSlot()
+      .accounts({ inbox: inbox, slot: slotPda, admin: newOwner.publicKey })
+      .signers([newOwner])
+      .rpc();
 
-    const [excpected_pda] = anchor.web3.PublicKey.findProgramAddressSync([whitelistedSender.publicKey.toBuffer()], program.programId);
-    const account_info = await provider.connection.getAccountInfo(excpected_pda);
-    expect(account_info).to.be.null;
+    const [expectedPda] = anchor.web3.PublicKey.findProgramAddressSync(
+      [whitelistedSender.publicKey.toBuffer()],
+      program.programId
+    );
+    const accountInfo = await provider.connection.getAccountInfo(expectedPda);
+    expect(accountInfo).to.be.null;
   });
 
   it("Admin can withdraw inbox balance without destroying the inbox", async () => {
-    // transfer the inbox some sol just in case
-    let token_airdrop = await provider.connection.requestAirdrop(inbox, 1000000000);
-    await provider.connection.confirmTransaction(token_airdrop);
-    const initialInboxBalance = await program.provider.connection.getBalance(inbox);
+    await airdropSol(inbox, 1000000000);
+    const initialInboxBalance = await provider.connection.getBalance(inbox);
 
-    await program
-        .methods
-        .withdrawSurplusInboxBalance()
-        .accounts({inbox: inbox, admin: newOwner.publicKey})
-        .signers([newOwner])
-        .rpc();
+    await program.methods
+      .withdrawSurplusInboxBalance()
+      .accounts({ inbox: inbox, admin: newOwner.publicKey })
+      .signers([newOwner])
+      .rpc();
 
-    const finalInboxBalance = await program.provider.connection.getBalance(inbox);
+    const finalInboxBalance = await provider.connection.getBalance(inbox);
     expect(finalInboxBalance).to.be.lessThan(initialInboxBalance);
   });
 });
